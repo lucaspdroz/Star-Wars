39f34bf0086d7a85d949755180cb638f6616ebda
Lucas <lucaspdroz@gmail.com>
2020-10-31 17:38:17 -0300

    fix cors

--- src/components/ListApi/index.js
+++ src/components/ListApi/index.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Character } from '../Character'
 
-axios.create({
+const api = axios.create({
     baseURL:"https://swapi.dev/api/"
 })
 
 const ListPeople = () => {
     const [listCharacters, setListCharacters] = useState([]);
     useEffect(() => {
-        axios.get('https://swapi.dev/api/people/1')
+        api.get('/people/1')
             .then(response => {
                 console.log(response.data);
                 setListCharacters(response.data);
