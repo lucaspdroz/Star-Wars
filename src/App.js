98cdf4317642624d4f5f4e156e30afa467c3f9dc
Lucas <lucaspdroz@gmail.com>
2020-10-31 16:43:25 -0300

    first commit, get API data /people/1 and list it on the screen

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './Styles/App.css';
+import ListPeople from './components/ListApi';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ListPeople></ListPeople>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ListApi/index.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Character } from '../Character'
+
+const ListPeople = () => {
+    const [listCharacters, setListCharacters] = useState([]);
+    useEffect(() => {
+        axios.get('https://swapi.dev/api/people/1')
+            .then(response => {
+                console.log(response.data);
+                setListCharacters(response.data);
+            })
+    }, [])
+
+        return (
+            <div className="wrapper">
+                <h1>My Character List</h1>
+                <Character character={listCharacters} />
+            </div>
+        )
+    }
+
+    export default ListPeople
